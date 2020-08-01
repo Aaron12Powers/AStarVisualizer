52d92a029be42c7d3c8e1f85fa7e73b189cd040c
Aaron PC <aaronpowers12@gmail.com>
2020-07-31 20:30:30 -0700

    write base pathfinding code

--- AStarVisualizer.py
+++ AStarVisualizer.py
@@ -4,23 +4,27 @@ from random import randint
 import pygame
 import module
 
+blank = False
+rotateRules = False
+randomize = False
 
-rule = 30
-rotateRules = True
 
 #Define Number of Columns
-columns = 159
-rows = 75
-
+columns = 30
+rows = 30
 #Define Cell Sizes
-WIDTH = 10
-HEIGHT = 10
+WIDTH = 20
+HEIGHT = 20
 MARGIN = WIDTH // 5
 
 #Define Colors
-WHITE = (155, 155, 155)
-BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+LIGHT_GREY = (155, 155, 155)
 GREY = (128, 128, 128)
+BLACK = (0, 0, 0)
+GREEN = (0,128,0)
+RED = (255,0,0)
+BLUE = (0, 0, 255)
 
 
 
@@ -32,20 +36,21 @@ for row in range(0, rows):
     for column in range(0, columns):
         cells[column][row]  = (module.Cell(row, column))
 
-for row in range(0, rows):
-    for column in range(0, columns):
-        print(cells[column][row].row,cells[column][row].column)
 
 window_height = (((HEIGHT + MARGIN) * rows) + MARGIN) 
 window_width = (((WIDTH + MARGIN) * columns) + MARGIN)
 
 screen = pygame.display.set_mode((window_width, window_height))
 
-pygame.display.set_caption('Cellular Automata - Rule ' + str(rule))
+pygame.display.set_caption('Conways Game of Life')
 
+if randomize:
+    module.StartRandom(cells, rows, columns)
+elif blank:
+    module.StartBlank(cells, rows, columns)
+else:
+    module.StartSim(cells, rows, columns)
 
-module.StartSim(cells, rows, columns)
-module.RunSim(cells, rows, columns, rule)
 
 grid = []
 for row in range(6):
@@ -57,86 +62,174 @@ for row in range(6):
 
 running = True
 
+
 clock = pygame.time.Clock()
 
 tick_count = 0
 
-row_step = 0
+step = 0
 
-pause = False
+pause = True
 
 speed = 41
 
+currentCoords = []
+
+screen.fill(GREY)
+for row in range(0, rows):
+    for column in range(0, columns):
+        if cells[column][row].color == 0:
+            color = WHITE
+        elif cells[column][row].color == 1:
+            color = BLACK
+        else:
+            color = GREY
+        pygame.draw.rect(screen,
+            color,
+            [(MARGIN + WIDTH) * cells[column][row].column + MARGIN,
+            (MARGIN + HEIGHT) * cells[column][row].row + MARGIN,
+            WIDTH, HEIGHT])
 #Main pygame loop
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:  
             running = False  # Exit loop/ close pygame when x is clicked
         key=pygame.key.get_pressed()  #checking pressed keys
-        if key[pygame.K_RETURN]: 
-            row_step = 0
-        if key[pygame.K_SPACE]:
-            if pause: pause = False
-            else: pause = True 
-        if key[pygame.K_DOWN]:
-            speed = int(speed * 2)
-        if key[pygame.K_UP]:
-            if speed > 1:
-                speed = int(speed * .75)
+        if key[pygame.K_RETURN]:
+            if randomize:
+                module.StartRandom(cells, rows, columns)
+            else:
+                module.StartSim(cells, rows, columns)
 
-        if key[pygame.K_LEFT]:
-            rule -= 1
-        if key[pygame.K_RIGHT]:
-            rule += 1
+        elif key[pygame.K_SPACE]:
+            if pause:
+                pause = False
+            else:
+                pygame.display.set_caption('Conways Game of Life - PAUSED')
+                pause = True 
 
+        elif event.type == pygame.MOUSEBUTTONDOWN and pause:
+            # If Left Click Make wall
+            if event.button == 1:
+                pos = pygame.mouse.get_pos()
+                column = pos[0] // (WIDTH + MARGIN)
+                row = pos[1] // (HEIGHT + MARGIN)
+                module.SetWall(cells[column][row])
+
+            # If Right click make End
+            elif event.button == 2:
+                pos = pygame.mouse.get_pos()
+                column = pos[0] // (WIDTH + MARGIN)
+                row = pos[1] // (HEIGHT + MARGIN)
+                module.SetEnd(cells[column][row])
+
+            # If Center Click Set Start
+            elif event.button == 3:
+                pos = pygame.mouse.get_pos()
+                column = pos[0] // (WIDTH + MARGIN)
+                row = pos[1] // (HEIGHT + MARGIN)
+                module.SetStart(cells[column][row])
+            
 
 
     
-
-    screen.fill(GREY)
-
-    #update next row every 20 ticks
-    if not pause:
-        if (tick_count) % speed == 0:
-            row_step += 1
-
-    for row in range(0, rows):
-        for column in range(0, columns):
-            if row <= row_step:
+    # Setup Initial Board
+    if tick_count % 10 == 0 and pause:
+        for row in range(0, rows):
+            for column in range(0, columns):
+                #if Path
                 if cells[column][row].color == 0:
-                    color = WHITE
+                    color = LIGHT_GREY
+                #if Start
                 elif cells[column][row].color == 1:
-                    color = BLACK
+                    color = GREEN
+                #if End
+                elif cells[column][row].color == 2:
+                    color = RED
+                #if Wall
+                elif cells[column][row].color == 3:
+                    color = BLACK    
+                #if Current Cell                                                          
+                elif cells[column][row].color == 4:
+                    color = BLUE 
                 else:
-                    color = GREY
+                    color = WHITE
                 pygame.draw.rect(screen,
                     color,
                     [(MARGIN + WIDTH) * cells[column][row].column + MARGIN,
                     (MARGIN + HEIGHT) * cells[column][row].row + MARGIN,
-                    WIDTH, HEIGHT])
-            else:
-                color = GREY
+                    WIDTH, HEIGHT]) 
+
+    # Find Start / End nodes / values
+    if tick_count % 2 == 0 and not pause and step == 0:
+        startCoords = module.findStart(cells, rows, columns)
+        currentCoords.append(startCoords[0])
+        currentCoords.append(startCoords[1])
+        endCoords = module.findEnd(cells, rows, columns)
+        totalDistance = module.findDistance(startCoords, endCoords)
+        cells[startCoords[0]][startCoords[1]].gCost = 0
+        cells[startCoords[0]][startCoords[1]].hCost = totalDistance
+        module.calculateFCost(cells[startCoords[0]][startCoords[1]])
+        for row in range(0, rows):
+            for column in range(0, columns):
+                cells[column][row].hCost = module.findDistance((column, row), endCoords)
+        step += 1
+
+    elif tick_count % 1 == 0 and not pause and step < (2 * rows * columns):
+        for i in range(-1, 2):
+            for j in range(-1, 2):
+                if not (i == 0 and j == 0):
+                    cells[currentCoords[0]+i][currentCoords[1]+j].hCost = module.findDistance((cells[currentCoords[0]+i][currentCoords[1]+j].column, cells[currentCoords[0]+i][currentCoords[1]+j].row), endCoords)                    
+                    if i == 0 or j == 0:
+                        cells[currentCoords[0]+i][currentCoords[1]+j].gCost = cells[currentCoords[0]][currentCoords[1]].gCost + 10
+                    else:
+                        cells[currentCoords[0]+i][currentCoords[1]+j].gCost = cells[currentCoords[0]][currentCoords[1]].gCost + 14
+                    module.calculateFCost(cells[currentCoords[0]+i][currentCoords[1]+j])
+                    # if cells[currentCoords[0]+i][currentCoords[1]+j].hCost >= totalDistance:
+                    #     cells[currentCoords[0]+i][currentCoords[1]+j].color = 3
+                    # else:
+                    #     cells[currentCoords[0]+i][currentCoords[1]+j].color = 0
+
+        screen.fill(GREY)
+        for row in range(0, rows):
+            for column in range(0, columns):
+                #if Path
+                if cells[column][row].color == 0:
+                    color = LIGHT_GREY
+                #if Start
+                elif cells[column][row].color == 1:
+                    color = GREEN
+                #if End
+                elif cells[column][row].color == 2:
+                    color = RED
+                #if wall
+                elif cells[column][row].color == 3:
+                    color = BLACK       
+                #if Current Cell
+                elif cells[column][row].color == 4:
+                    color = BLUE                                                      
+                else:
+                    color = WHITE
                 pygame.draw.rect(screen,
                     color,
                     [(MARGIN + WIDTH) * cells[column][row].column + MARGIN,
                     (MARGIN + HEIGHT) * cells[column][row].row + MARGIN,
-                    WIDTH, HEIGHT])
+                    WIDTH, HEIGHT]) 
 
+        currentCoordsNew = []
+        currentCoordsNew.append(module.ChooseNext(cells, rows, columns,currentCoords)[0])
+        currentCoordsNew.append(module.ChooseNext(cells, rows, columns,currentCoords)[1])
+        currentCoords = currentCoordsNew
+        if cells[currentCoords[0]][currentCoords[1]].color != 1:
+            cells[currentCoords[0]][currentCoords[1]].color = 4
+        step += 1
 
-    if row_step == rows:
-        row_step = 0
-        if rotateRules:
-            if rule <= 255:
-                rule += 1
-                pygame.display.set_caption('Cellular Automata - Rule ' + str(rule))
-                module.StartSim(cells, rows, columns)
-                module.RunSim(cells, rows, columns, rule)
-            else:
-                rule = 0
+        if currentCoords == list(endCoords):
+            print('END FOUND')
+            step = (2 * rows * columns) + 1
         else:
-            pygame.display.set_caption('Cellular Automata - Rule ' + str(rule))
-            module.StartSim(cells, rows, columns)
-            module.RunSim(cells, rows, columns, rule)
+            print(currentCoords, endCoords)
+
     tick_count += 1
     clock.tick(60)
    
--- module.py
+++ module.py
@@ -1,152 +1,131 @@
+import random
+import math
 
 '''
-Colors... 
-White = 0
-Black = 1
-Grey = 2
+WHITE = 5
+LIGHT_GREY = 0
+GREY = (128, 128, 128)
+BLACK = 3
+GREEN = 1
+RED = 2
+BLUE = 4
 '''
+
+
 class Cell(object):
     def __init__(self, row, column):
         self.row = row
         self.column = column
-        self.color = 2
+        self.color = 5
+        self.isStart = 0
+        self.isEnd = 0
+        self.isWall = 0
+        self.isPath = 0
+        self.gCost = 9999
+        self.hCost = 9999
+        self.fCost = 9999
 
 #setup first cell of simulation
-def StartSim(cells,rows, columns):
+def StartSim(cells, rows, columns):
     for row in range(0, rows):
         for column in range(0, columns):
-            if cells[column][row].column != (columns // 2):
-                cells[column][row].color = 0
-            elif cells[column][row].row != 0:
-                cells[column][row].color = 0    
-            else:
-                cells[column][row].color = 1
+            cells[column][row].color = 5
 
-def RunSim(cells, rows, columns, rule):
-    for row in range(1, rows):
+
+def StartRandom(cells, rows, columns):
+    for row in range(0, rows):
         for column in range(0, columns):
-            ruleNumber = rule
-            if ruleNumber >= 128:
-                ruleNumber = ruleNumber - 128
-                if RuleEight(cells, row, column):
-                    cells[column][row].color = 1
-                    continue
-            if ruleNumber >= 64:
-                ruleNumber = ruleNumber - 64
-                if RuleSeven(cells, row, column):
-                    cells[column][row].color = 1
-                    continue
-            if ruleNumber >= 32:
-                ruleNumber = ruleNumber - 32
-                if RuleSix(cells, row, column):
-                    cells[column][row].color = 1
-                    continue
-            if ruleNumber >= 16:
-                ruleNumber = ruleNumber - 16
-                if RuleFive(cells, row, column):
-                    cells[column][row].color = 1
-                    continue
-            if ruleNumber >= 8:
-                ruleNumber = ruleNumber - 8
-                if RuleFour(cells, row, column):
-                    cells[column][row].color = 1
-                    continue
-            if ruleNumber >= 4:
-                ruleNumber = ruleNumber - 4
-                if RuleThree(cells, row, column):
-                    cells[column][row].color = 1
-                    continue
-            if ruleNumber >= 2:
-                ruleNumber = ruleNumber - 2
-                if RuleTwo(cells, row, column):
-                    cells[column][row].color = 1
-                    continue
-            if ruleNumber >= 1:
-                ruleNumber = ruleNumber - 1
-                if RuleOne(cells, row, column):
-                    cells[column][row].color = 1
-                    continue
-            
+            randomRoll = random.randint(0,100)
+            if randomRoll % 5 == 0:
+                cells[column][row].color = 1
+            else:
+                cells[column][row].color = 0
 
-    print(columns, rows)
-    cells[columns - 1][rows-1].color = 2
+def StartBlank(cells, rows, columns):
+    for row in range(0, rows):
+        for column in range(0, columns):
+            cells[column][row].color = 0
 
-def RuleOne(cells, row, column):
-    print('RuleOne')
-    try:
-        if cells[column-1][row-1].color == 0 and cells[column][row-1].color == 0 and cells[column+1][row-1].color == 0:
-            return True
-        else:
-            return False
-    except:
-        return False
 
-def RuleTwo(cells, row, column):
-    print('RuleTwo')
-    try:
-        if cells[column-1][row-1].color == 0 and cells[column][row-1].color == 0 and cells[column+1][row-1].color == 1:
-            return True
-        else:
-            return False
-    except:
-        return False
+def RunSim(oldCells, newCells, rows, columns):
+    print('runsim')
 
-def RuleThree(cells, row, column):
-    print('RuleThree')
-    try:
-        if cells[column-1][row-1].color == 0 and cells[column][row-1].color == 1 and cells[column+1][row-1].color == 0:
-            return True
-        else:
-            return False
-    except:
-        return False
+def SetPath(cell):
+    cell.isWall = True
+    cell.isStart = False
+    cell.isEnd = False
+    cell.isWall = False
+    cell.color = 0
 
-def RuleFour(cells, row, column):
-    print('RuleFour')
-    try:
-        if cells[column-1][row-1].color == 0 and cells[column][row-1].color == 1 and cells[column+1][row-1].color == 1:
-            return True
-        else:
-            return False
-    except:
-        return False
+def SetStart(cell):
+    cell.isWall = False
+    cell.isStart = True
+    cell.isEnd = False
+    cell.isWall = False
+    cell.color = 1
 
-def RuleFive(cells, row, column):
-    print('RuleFive')
-    try:
-        if cells[column-1][row-1].color == 1 and cells[column][row-1].color == 0 and cells[column+1][row-1].color == 0:
-            return True
-        else:
-            return False
-    except:
-        return False
+def SetEnd(cell):
+    cell.isWall = True
+    cell.isStart = False
+    cell.isEnd = True
+    cell.isWall = False
+    cell.color = 2
 
-def RuleSix(cells, row, column):
-    print('RuleSix')
-    try:
-        if cells[column-1][row-1].color == 1 and cells[column][row-1].color == 0 and cells[column+1][row-1].color == 1:
-            return True
-        else:
-            return False
-    except:
-        return False
+def SetWall(cell):
+    cell.isStart = False
+    cell.isEnd = False
+    if cell.isWall == False:
+        cell.isWall = True
+        cell.color = 3
+    else:
+        cell.isWall = False
+        cell.color = 5
+    
 
-def RuleSeven(cells, row, column):
-    try:
-        print('RuleSeven')
-        if cells[column-1][row-1].color == 1 and cells[column][row-1].color == 1 and cells[column+1][row-1].color == 0:
-            return True
-        else:
-            return False
-    except:
-        return False
+def findStart(cells, rows, columns):
+    for row in range(rows):
+        for column in range(columns):
+            if cells[column][row].isStart:
+                return (cells[column][row].column, cells[column][row].row)
 
-def RuleEight(cells, row, column):
-    print('RuleEight')
-    try:
-        if cells[column-1][row-1].color == 1 and cells[column][row-1].color == 1 and cells[column+1][row-1].color == 1:
-            return True
-        else:
-            return False
-    except:
-        return False
+def findEnd(cells, rows, columns):
+    for row in range(rows):
+        for column in range(columns):
+            if cells[column][row].isEnd:
+                return (cells[column][row].column, cells[column][row].row)
+
+def findDistance(start, end):
+    column = abs(start[0] - end[0])
+    row = abs(start[1] - end[1])
+    return int(math.sqrt((row * row) + (column * column)) * 10)
+
+
+def calculateFCost(cell):
+    cell.fCost = cell.hCost + cell.gCost
+
+def ChooseNext(cells, rows, columns, currentCoords):
+    currentLow = 999999
+    currentLowCell = [1, 1]
+    for row in range(rows):
+        for column in range(columns):
+            if cells[column][row].isWall: 
+                continue
+            if cells[column][row].color == 4 or cells[column][row].color == 1:
+                continue
+            for i in range(rows):
+                for j in range(columns):
+                    if cells[j][i].color == 4 or cells[j][i].color == 1:
+                        if findDistance((j, i), (column, row)) <= 14 and findDistance((j, i), (column, row)) <= 14:
+                            cells[column][row].gCost = findDistance((j, i), (column, row)) + cells[j][i].gCost
+                            calculateFCost(cells[column][row])
+                            print(cells[column][row].gCost)
+
+            if cells[column][row].fCost < currentLow:
+                # print(currentCoords, (column, row))
+                if tuple(currentCoords) != (column, row):
+                    currentLow = cells[column][row].fCost
+                    currentLowCell = []
+                    currentLowCell.append(column)
+                    currentLowCell.append(row)
+    return currentLowCell
+        
